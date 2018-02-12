31d71c9d411907247fbf741e730bdda3e3a6a318
Jon Benson <bensojon@gmail.com>
2018-02-11 20:58:50 -0800

    Fix outputs

--- main.tf
+++ main.tf
@@ -80,7 +80,7 @@ module "consul_client_sg" {
 }
 
 resource "aws_security_group_rule" "ssh" {
-  security_group_id = "${module.vault_server_sg.vault_server_sg_id}"
+  security_group_id = "${element(module.vault_server_sg.vault_server_sg_id, 0)}"
   type              = "ingress"
   protocol          = "tcp"
   from_port         = 22
@@ -91,15 +91,15 @@ resource "aws_security_group_rule" "ssh" {
 resource "aws_launch_configuration" "vault_server" {
   associate_public_ip_address = "${var.public_ip != "false" ? true : false}"
   ebs_optimized               = false
-  iam_instance_profile        = "${var.instance_profile != "" ? var.instance_profile : module.consul_auto_join_instance_role.instance_profile_id}"
+  iam_instance_profile        = "${var.instance_profile != "" ? var.instance_profile : element(module.consul_auto_join_instance_role.instance_profile_id, 0)}"
   image_id                    = "${var.image_id != "" ? var.image_id : data.aws_ami.vault.id}"
   instance_type               = "${var.instance_type}"
   user_data                   = "${data.template_file.vault_init.rendered}"
   key_name                    = "${var.ssh_key_name}"
 
   security_groups = [
-    "${module.vault_server_sg.vault_server_sg_id}",
-    "${module.consul_client_sg.consul_client_sg_id}",
+    "${element(module.vault_server_sg.vault_server_sg_id, 0)}",
+    "${element(module.consul_client_sg.consul_client_sg_id, 0)}",
   ]
 
   lifecycle {
