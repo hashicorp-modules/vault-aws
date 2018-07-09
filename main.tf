bffc9044c420d42c3b8db846dd84403d27353782
Matt Keeler <mkeeler@algebraix.io>
2018-07-09 17:24:21 -0500

    Add support for specifying root volume details

--- main.tf
+++ main.tf
@@ -116,6 +116,12 @@ resource "aws_launch_configuration" "vault" {
   user_data                   = "${data.template_file.vault_init.rendered}"
   key_name                    = "${var.ssh_key_name}"
 
+  root_block_device = {
+    volume_type = "${var.volume_type}",
+    volume_size = "${var.volume_size}",
+    delete_on_termination = "${var.volume_delete_on_termination}",
+  }
+
   security_groups = [
     "${module.vault_server_sg.vault_server_sg_id}",
     "${module.consul_client_sg.consul_client_sg_id}",
